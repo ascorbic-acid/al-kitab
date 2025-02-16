4e71e40024abdbedbd69d797e565afda7d5fb25d
Imad Abdou <igentle.appletec@gmail.com>
2025-02-16 13:34:47 +0000

    v1.2.2

--- app/components/App/AppDrawer.vue
+++ app/components/App/AppDrawer.vue
@@ -7,7 +7,7 @@
             <div style="display: flex; justify-content: space-between;">
               <div>
                 <span class="text-primary">الكتاب</span>
-                <span style="font-size: 8px; font-weight: 100; margin: 5px;">v1.2.1</span>
+                <span style="font-size: 8px; font-weight: 100; margin: 5px;">v1.2.2</span>
               </div>
             </div>
           </v-list-item-title>
